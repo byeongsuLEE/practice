8c9a2dd49872b202c00a1592cea1fda7c85f20bf
lb <75319125+byeongsuLEE@users.noreply.github.com>
2026-01-09 15:18:01 +0900

    init : 환경설정 세팅

---
+++ src/test/java/com/lbs/blaybus/BlaybusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbs.blaybus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlaybusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
