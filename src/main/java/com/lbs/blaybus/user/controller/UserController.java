43feb54814c3fcecee9deba9ad7f81d6fe047e5d
lb <75319125+byeongsuLEE@users.noreply.github.com>
2026-01-10 11:35:03 +0900

    test : test url

--- src/main/java/com/lbs/blaybus/user/controller/UserController.java
+++ src/main/java/com/lbs/blaybus/user/controller/UserController.java
@@ -1,8 +1,18 @@
 package com.lbs.blaybus.user.controller;
 
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @GetMapping("/test")
+    public String test() {
+
+        return "test";
+    }
     // TODO: Add controller methods
 }
